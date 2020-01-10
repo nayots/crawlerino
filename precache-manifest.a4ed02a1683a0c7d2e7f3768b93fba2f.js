505ae0f3c9cbfe5c2e5676dd9dde5456c795b236
Stoyan Grigorov <grigorov.stoyan@gmail.com>
2020-01-10 21:39:50 +0200

    Updates

---
+++ precache-manifest.a4ed02a1683a0c7d2e7f3768b93fba2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2721680cc7e51c1336f7f62968d0c81f",
+    "url": "/crawlerino/index.html"
+  },
+  {
+    "revision": "e88196226d8dddd0640a",
+    "url": "/crawlerino/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "7c2050d74000417cf240",
+    "url": "/crawlerino/static/js/2.53730dbb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/crawlerino/static/js/2.53730dbb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e88196226d8dddd0640a",
+    "url": "/crawlerino/static/js/main.e5d77439.chunk.js"
+  },
+  {
+    "revision": "e73b16b0c4ce412c8739",
+    "url": "/crawlerino/static/js/runtime-main.74dde007.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crawlerino/static/media/logo.5d5d9eef.svg"
+  }
+]);
